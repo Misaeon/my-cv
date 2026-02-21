c8cb1baf6906e071e5a48e3ef47d5cb475e1b41a
Misaeon <117101931+Misaeon@users.noreply.github.com>
2026-02-21 22:27:04 +0000

    Deploying to gh-pages from @ Misaeon/my-cv@aa51274bb695aec0e98985a03b14a053cbb12f22 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/my-cv/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/my-cv/people/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
