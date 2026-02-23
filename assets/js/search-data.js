2f1c556922f7f3dd57ad6ca8590ee1a12a8cdeac
Misaeon <117101931+Misaeon@users.noreply.github.com>
2026-02-23 22:58:31 +0000

    Deploying to gh-pages from @ Misaeon/my-cv@443f06125984f7fa162c7325df8d2cfe1af7d242 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/my-cv/teaching/";
